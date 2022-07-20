c69f53d80f6022270b09342ff412d5d39c5f0138
Reinaldo Correia <santosdoscorreiareinaldo@gmail.com>
2022-07-20 20:27:35 -0300

    Componente: Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,15 @@
+import { Flex } from '@chakra-ui/react';
 import { Header } from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
 
 export default function Dashboard() {
   return (
-    <>
+    <Flex direction="column" height="100vh">
       <Header />
-      <h1>Dashboard</h1>
-    </>
+      <Flex width="100%" marginY={6} maxW="1480px" marginX="auto" paddingX={6}>
+        <Sidebar />
+        <h1>teste</h1>
+      </Flex>
+    </Flex>
   )
 }
